11a1244733b27bd1d3ae4f073dfcdd80b273b07f
Mangaldip Ghosh <87035507+ghoshmangaldip124@users.noreply.github.com>
2022-12-10 15:00:15 +0530

    Add files via upload

---
+++ CORE_JAVA/Squer.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Squer{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Your Number :- ");
+		double x=sc.nextDouble(),y;
+		y=x*x;
+		System.out.print("X value is :- "+y);
+	}
+}
